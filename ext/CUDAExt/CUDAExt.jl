4b86dfbe7484b76d71db822ddeb4bb26e71e203f
Riteshbhirud <riteshbhirud@gmail.com>
2025-08-27 03:41:51 -0400

    PART1_GPU

--- ext/CUDAExt/CUDAExt.jl
+++ ext/CUDAExt/CUDAExt.jl
@@ -2,7 +2,6 @@ module CUDAExt
 
 using CUDA
 using CUDA: CuArray, CuVector, CuMatrix, @cuda, threadIdx, blockIdx, blockDim, gridDim
-
 using Gabs
 using Gabs: SymplecticBasis, QuadPairBasis, QuadBlockBasis
 using Gabs: GaussianState, GaussianUnitary, GaussianChannel
@@ -11,13 +10,9 @@ using Gabs: _vacuumstate, _coherentstate, _squeezedstate, _thermalstate, _eprsta
 using Gabs: _displace, _squeeze, _twosqueeze, _phaseshift, _beamsplitter
 using Gabs: _attenuator, _amplifier
 using Gabs: symplecticform, WIGNER_ERROR
-
 using LinearAlgebra: I, det, mul!, eigvals, Diagonal, logdet, dot, inv
 using Random: randn!
-
-# Check CUDA availability and provide graceful fallbacks
 const CUDA_AVAILABLE = CUDA.functional()
-
 function __init__()
     if CUDA_AVAILABLE
         @info "CUDA.jl extension loaded successfully. GPU acceleration enabled."
@@ -25,10 +20,9 @@ function __init__()
         @warn "CUDA not available. GPU operations will fall back to CPU."
     end
 end
-
 include("utils.jl")
 include("state_operations.jl") 
 include("unitary_operations.jl")
 include("wigner_kernels.jl")
 
-end # module CUDAExt
+end 
